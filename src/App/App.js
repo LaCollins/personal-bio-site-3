5eae0907197ad99d94fea7bfc102d62f49f51b5a
Laura Collins <laura.e.collins1@gmail.com>
2020-08-30 10:37:52 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.scss';
+import {
+  ThemeProvider, createTheme, Arwes, Button, Header, Heading,
+  Row, Col,
+} from 'arwes';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <ThemeProvider theme={createTheme()}>
+          <Arwes animate show>
+            {anim => (
+              <div style={{ padding: 20 }}>
+              <Header animate>
+                <Row>
+                <Col s={12} m={8} l={8} xl={8}>
+                <Heading animate show={anim.entered}>
+                  <h1 style={{ margin: 0 }}>Test</h1>
+                </Heading>
+                </Col>
+                <Col s={12} m={4} l={4} xl={4} className="buttonContainer">
+                  <Button animate show={anim.entered} style={{ margin: 10 }}>Test</Button>
+                </Col>
+                </Row>
+              </Header>
+            </div>
+            )}
+          </Arwes>
+        </ThemeProvider>
+    );
+  }
 }
 
 export default App;
